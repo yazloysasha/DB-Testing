bdabb37e270b25c97d3f2faf2244d66668f37ea8
yazloysasha <yazloysasha@gmail.com>
2024-10-26 23:17:36 +0700

    Добавлены заявления на расторжение брака

--- src/services/database/helpers/cro/tables/divorceAppications.ts
+++ src/services/database/helpers/cro/tables/divorceAppications.ts
@@ -1,8 +1,32 @@
+import { ApplicationType } from "@types";
 import { TableFunction } from "@services";
 
 /**
  * Заявления на расторжение брака
  */
-export const divorceApplicationsTableFunction: TableFunction = async () => {
-  return {};
+export const divorceApplicationsTableFunction: TableFunction = async (sql) => {
+  // Получить случайное заявление на расторжение брака и случайный брак
+  const result = await Promise.all([
+    sql`SELECT id FROM applications WHERE type = ${ApplicationType.DIVORCE} ORDER BY RANDOM() LIMIT 1`,
+    sql`SELECT wifeId, husbandId FROM "marriageApplications" ORDER BY RANDOM() LIMIT 1`,
+  ]);
+
+  const application = result[0][0] as { id: number };
+  const marriageApplication = result[1][0] as {
+    wifeId: number;
+    husbandId: number;
+  };
+
+  if (!application) {
+    throw Error(
+      `Table "applications" with "${ApplicationType.MARRIAGE}" type is empty`
+    );
+  }
+  if (!marriageApplication) throw Error('Table "marriageApplication" is empty');
+
+  return {
+    applicationId: String(application.id),
+    wifeId: String(marriageApplication.wifeId),
+    husbandId: String(marriageApplication.husbandId),
+  };
 };
