d57b2162b3f168e7ac760768258541c04015bf02
yazloysasha <yazloysasha@gmail.com>
2024-10-27 00:52:56 +0700

    Добавлены заявления на смерть

--- src/services/database/helpers/cro/tables/deathApplications.ts
+++ src/services/database/helpers/cro/tables/deathApplications.ts
@@ -1,8 +1,31 @@
+import { ApplicationType } from "@types";
 import { TableFunction } from "@services";
+import { getRandomAddress, getRandomAltDate } from "@utils";
 
 /**
  * Заявления на смерть
  */
-export const deathApplicationsTableFunction: TableFunction = async () => {
-  return {};
+export const deathApplicationsTableFunction: TableFunction = async (sql) => {
+  // Получить случайное заявление на смерть и случайного человека
+  const result = await Promise.all([
+    sql`SELECT id FROM applications WHERE type = ${ApplicationType.DEATH} ORDER BY RANDOM() LIMIT 1`,
+    sql`SELECT id FROM people ORDER BY RANDOM() LIMIT 1`,
+  ]);
+
+  const application = result[0][0] as { id: number };
+  const person = result[1][0] as { id: number };
+
+  if (!application) {
+    throw Error(
+      `Table "applications" with "${ApplicationType.DEATH}" type is empty`
+    );
+  }
+  if (!person) throw Error('Table "people" is empty');
+
+  return {
+    applicationId: String(application.id),
+    personId: String(person.id),
+    personDeathPlace: getRandomAddress(),
+    personDeathDate: getRandomAltDate("now"),
+  };
 };
